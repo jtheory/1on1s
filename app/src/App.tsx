2825730789f9c79f2379735c4e9deb41813a518a
Rob Whelan <hello@robwhelan.com>
2022-01-03 10:43:27 +0100

    fix tsc warning

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from 'react'
+import React from 'react'
 import logo from './logo.svg'
 // import forest from './forest.svg'
 import contentJson from './__generated__/content.json'
